3235171a6ae74fe65e911a5ca961b507a50424ec
fore10der <qovalski@gmail.com>
2023-01-29 02:51:38 +0600

    write a sample code

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,15 @@
-console.log('it launched')
+export class Counter {
+   #value: number = 0
+
+    increase() {
+       this.#value++
+    }
+
+    decrease() {
+       this.#value--
+    }
+
+    getValue() {
+       return this.#value
+    }
+}
